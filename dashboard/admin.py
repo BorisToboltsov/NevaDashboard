8cb238d1864952f9a5a05011a5b35395fd14a9cc
Toboltsov Boris <Gh0st184@vcore.su>
2022-01-12 15:35:08 +0300

    admin.site.register models and created media folder

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import CatalogName, CatalogData, Contact, RegistrySendingTravelAgency, RegistryTransportOrganizations, RegistryFoodOrganizations, RegistryHotels, RegistryMuseum, EmployeeData, Group, GroupMuseum, GroupHotel, GroupTransport, GroupFood
 
 # Register your models here.
+
+admin.site.register(CatalogName)
+admin.site.register(CatalogData)
+admin.site.register(Contact)
+admin.site.register(RegistrySendingTravelAgency)
+admin.site.register(RegistryTransportOrganizations)
+admin.site.register(RegistryFoodOrganizations)
+admin.site.register(RegistryHotels)
+admin.site.register(RegistryMuseum)
+admin.site.register(EmployeeData)
+admin.site.register(Group)
+admin.site.register(GroupMuseum)
+admin.site.register(GroupHotel)
+admin.site.register(GroupTransport)
+admin.site.register(GroupFood)
