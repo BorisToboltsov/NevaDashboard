be7413b401219cee0dbd0a4dc27e409f929de4d0
Gh0st184 <tu5s0qg>
2022-01-25 21:16:40 +0300

    check_in and menu css

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,8 +8,30 @@ from dashboard.models import Group
 
 
 def check_in(request):
+    if request.method == 'POST':
+        form = DashboardDate(data=request.POST)
+        start_date = datetime.datetime.strptime(request.POST.get('start_date'), '%d-%m-%Y').date()
+        end_date = datetime.datetime.strptime(request.POST.get('end_date'), '%d-%m-%Y').date()
+        data_range = end_date - start_date
+        dates = list()
+        for days in range(0, data_range.days + 1):
+            dates.append((start_date + datetime.timedelta(days)).isoformat())
+        groups = Group.objects.values()  # Выбор всех групп
+        # groups = Group.objects.filter(arrival_date__gte=start_date).filter(departure_date__lte=end_date).values()
+        for i in groups:
+            i['arrival_date'] = i['arrival_date'].date().isoformat()
+            i['departure_date'] = i['departure_date'].date().isoformat()
+
+    else:
+        form = DashboardDate()
+        dates = list()
+        groups = []
     context = {
         'title': 'Dashboard график заездов',
+        'url': 'dashboard:check_in',
+        'form': form,
+        'dates': dates,
+        'groups': groups,
     }
     return render(request, 'dashboard/check_in.html', context)
 
